868abcf5399ae3240ddaf9211463e1446974fc6f
Maria Eduarda Frota <mariadamascenafrota@gmail.com>
2022-02-19 16:25:33 -0300

    Update codificacao.js

--- codificacao.js
+++ codificacao.js
@@ -2,45 +2,45 @@ var texto;
 var operacao;
 var incremento;
 function cifrarCesar() {
-  incremento = parseInt(document.getElementById("incremento").value);
-  texto = texto.toUpperCase();
-  let textoCodificado = "";
-  Array.from(texto).forEach(function (t) {
-    if (t == " ") {
-      textoCodificado = textoCodificado + t;
-    } else {
-      let codigoAscii = t.charCodeAt();
-      if (codigoAscii > 64 && codigoAscii < 91) {
-        let posicao = codigoAscii + incremento;
-        if (posicao > 90) {
-          posicao = posicao - 90 + 64;
+    incremento = parseInt(document.getElementById("incremento").value);
+    texto = texto.toUpperCase();
+    let textoCodificado = "";
+    Array.from(texto).forEach(function (t) {
+        if (t == " ") {
+            textoCodificado = textoCodificado + t;
+        } else {
+            let codigoAscii = t.charCodeAt();
+            if (codigoAscii > 64 && codigoAscii < 91) {
+                let posicao = codigoAscii + incremento;
+                if (posicao > 90) {
+                    posicao = posicao - 90 + 64;
+                }
+                textoCodificado += String.fromCharCode(posicao);
+            }
         }
-        textoCodificado += String.fromCharCode(posicao);
-      }
-    }
-  });
-  return textoCodificado;
+    });
+    return textoCodificado;
 }
 
 function decifrarCesar() {
-  incremento = parseInt(document.getElementById("incremento").value);
-  texto = texto.toUpperCase();
-  let textoDecodificado = "";
-  Array.from(texto).forEach(function (t) {
-    if (t == " ") {
-      textoDecodificado = textoDecodificado + t;
-    } else {
-      let codigoAscii = t.charCodeAt();
-      if (codigoAscii > 64 && codigoAscii < 91) {
-        let posicao = codigoAscii - incremento;
-        if (posicao <= 64) {
-          posicao = 90-(64-posicao);
+    incremento = parseInt(document.getElementById("incremento").value);
+    texto = texto.toUpperCase();
+    let textoDecodificado = "";
+    Array.from(texto).forEach(function (t) {
+        if (t == " ") {
+            textoDecodificado = textoDecodificado + t;
+        } else {
+            let codigoAscii = t.charCodeAt();
+            if (codigoAscii > 64 && codigoAscii < 91) {
+                let posicao = codigoAscii - incremento;
+                if (posicao <= 64) {
+                    posicao = 90 - (64 - posicao);
+                }
+                textoDecodificado += String.fromCharCode(posicao);
+            }
         }
-        textoDecodificado += String.fromCharCode(posicao);
-      }
-    }
-  });
-  return textoDecodificado;
+    });
+    return textoDecodificado;
 }
 function cifrarBase64() {
     return btoa(texto);
@@ -50,39 +50,37 @@ function decifrarBase64() {
 }
 // Atualiza o texto do botao
 function atualizarBotao(o) {
-  let botao = document.getElementById("executar");
-  if (o == "codificar") {
-    botao.innerText = "Codificar Mensagem!";
-  } else {
-    botao.innerText = "Decodificar Mensagem!";
-  }
-  operacao = o;
+    let botao = document.getElementById("executar");
+    if (o == "codificar") {
+        botao.innerText = "Codificar Mensagem!";
+    } else {
+        botao.innerText = "Decodificar Mensagem!";
+    }
+    operacao = o;
 }
 
 // Mostra a box de elemento se for do tipo Cifra de Cesar
 function atualizarTipo(tipo) {
-  let incremento = document.getElementById("boxIncremento");
-  incremento.hidden = tipo != "cesar";
+    let incremento = document.getElementById("boxIncremento");
+    incremento.hidden = tipo != "cesar";
 }
 
 function cifrar() {
-  texto = document.getElementById("texto").value;
+    texto = document.getElementById("texto").value;
     let tipo = document.getElementById("tipo").value;
-  var resultado;
-  if (tipo == "cesar") {
-      if (operacao == "codificar") {
-          resultado = cifrarCesar();
-      } else{
-         resultado = decifrarCesar();
-      }
-  } else {
-    if (operacao == "codificar") {
-        resultado = cifrarBase64();
-    } else{
-       resultado = decifrarBase64();
+    var resultado;
+    if (tipo == "cesar") {
+        if (operacao == "codificar") {
+            resultado = cifrarCesar();
+        } else {
+            resultado = decifrarCesar();
+        }
+    } else {
+        if (operacao == "codificar") {
+            resultado = cifrarBase64();
+        } else {
+            resultado = decifrarBase64();
+        }
     }
+    document.getElementById("resultado").innerHTML = resultado;
 }
-document.getElementById("resultado").innerHTML = resultado;
-
-}
-
